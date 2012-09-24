e060615c444c067112c8747c80c46b31acaff504
Niklas Vågstedt <niklasva@gmail.com>
2012-09-24 13:44:03 +0200

    Repository skapad
    NU SER JAG VAD DU KODAR JOEL

---
+++ ProjektArbete/ProjektArbete/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProjektArbete")]
+[assembly: AssemblyProduct("ProjektArbete")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Skövde kommun")]
+[assembly: AssemblyCopyright("Copyright © Skövde kommun 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("91855478-1c23-4083-a9dd-de9fc3b484d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
