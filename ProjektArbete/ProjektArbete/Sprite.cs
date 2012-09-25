a2021ef10f913d3584c2b22869db497c5ac219db
Niklas Vågstedt <niklasva@gmail.com>
2012-09-25 18:05:31 +0200

    +Sprite.cs
    Gjorde en liten spriteklass

---
+++ ProjektArbete/ProjektArbete/Sprite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProjektArbete
+{
+    class Sprite
+    {
+        public Texture2D Texture { get; set; }
+        public Vector2 Position {get; set;}
+        public Sprite(Texture2D texture, Vector2 position)
+        {
+            Texture = texture;
+            Position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
