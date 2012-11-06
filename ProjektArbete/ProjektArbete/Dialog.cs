860b7c0b289371e5720e13ae799df8715b4133c7
Niklas Vågstedt <niklasva@gmail.com>
2012-11-06 19:06:26 +0100

    Dialog

---
+++ ProjektArbete/ProjektArbete/Dialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ProjektArbete
+{
+    class Dialog
+    {
+        NPC npc;
+        String dialogID;
+        Player player;
+        List<string> lines = new List<string>();
+        //Konstruktor
+        public Dialog(Player player, NPC npc, String dialogID)
+        {
+            this.npc = npc;
+            this.dialogID = dialogID;
+            this.player = player;
+        }
+        public override void LoadContent()
+        {
+            using (StreamReader reader = new StreamReader(@"dialog.txt"))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line.StartsWith(dialogID))
+                    {
+                        line.Replace(dialogID, "");
+                        lines.Add(line);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
