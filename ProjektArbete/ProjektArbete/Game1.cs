79459ce34f86c06dde1bfcaf7285f0410d564106
Niklas Vågstedt <niklasva@gmail.com>
2013-04-25 19:47:28 +0200

    Musik, slut
    Lade till en massa musik
    Fixade Stephen jkea
    Gjorde slutet underbart!

--- ProjektArbete/ProjektArbete/Game1.cs
+++ ProjektArbete/ProjektArbete/Game1.cs
@@ -80,7 +80,7 @@ namespace ProjektArbete
             Registry.npcs = Content.Load<Library.NPC[]>(@"Data/npcs");              // Här händer viktiga saker. NPC-listan i registret skapas
             Registry.dialogs = Content.Load<Library.Dialog[]>(@"Data/dialogs");     // Dialoglistan i Registry skapas
             Registry.rooms = Content.Load<Library.Room[]>(@"Data/rooms");           // Rum i Registry skapas
-            Registry.currentRoom = Registry.rooms[24];                               // Startrummet
+            Registry.currentRoom = Registry.rooms[1];                               // Startrummet
             Registry.currentRoom.LoadContent(this);                                 // GO!
         }
 
@@ -156,7 +156,7 @@ namespace ProjektArbete
             }
             else if (!Registry.pause)
             {
-                MediaPlayer.Volume = 0.5f;             // Hög volym (MINA ÖRON)
+                MediaPlayer.Volume = 0.25f;             // Hög volym (MINA ÖRON)
             }
 
             mouseState = Mouse.GetState();
